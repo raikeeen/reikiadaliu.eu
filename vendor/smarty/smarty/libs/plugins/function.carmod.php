d55ff1b59f5d6bb105ec4dcfb76ef6689bbef941
raikeeen <sonnikcs@mail.ru>
2021-03-10 11:07:33 +0200

    mail

--- vendor/smarty/smarty/libs/plugins/function.carmod.php
+++ vendor/smarty/smarty/libs/plugins/function.carmod.php
@@ -18,7 +18,43 @@
  */
 function smarty_function_carmod( $params, $template)
 {
-    $SearchPosition = 'Main';
+    //Lp express terminal
+    $servername = "localhost";
+    $username = "Admin";
+    $password = "DFfgdde467%$#d";
+    $dbname = "reikiadaliu";
 
-    include_once($_SERVER['DOCUMENT_ROOT'] . '/carparts/add/search/default/template.php');
+// Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    //SELECT * from ps_orders WHERE ps_orders.reference = 'MCKLGYBMQ'
+    $sql = "SELECT Max(id_order) from ps_orders";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result);
+    $sql = "SELECT id_lpexpress_terminal FROM `ps_lpexpress_terminal_order` WHERE ps_lpexpress_terminal_order.id_order = ".$row['id_order'];
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result);
+    $sql = "SELECT name,address,zip,city FROM `ps_lpexpress_terminal` WHERE ps_lpexpress_terminal.id_lpexpress_terminal =".$row['id_lpexpress_terminal'];
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result);
+
+    print($row['name'] . " ". $row['address'] . " ". $row['zip']." ". $row['city']. "<br>");
+
+
+    $conn->close();
+    /*$sql = "INSERT INTO ps_lpexpress_terminal_order (id_cart, id_order, id_lpexpress_terminal,id_lpexpress_box,'type',weight,packets,cod,cod_amount)
+VALUES ($id_cart, $order->id, $terminal,0,'terminal',0,1,1,$amount_paid)";*/
+    /*$sql = "INSERT INTO ps_lpexpress_terminal_order (id_cart, id_order, id_lpexpress_terminal,id_lpexpress_box,type,weight,packets,cod,cod_amount)
+VALUES (1500, 1500, 260,0,'terminal',0,1,1,1500)";
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();*/
+
 }
